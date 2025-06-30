3303164f23a35b740cfe6165267ac36c384e128c
Marcelo Ito <50713742+GremlinX@users.noreply.github.com>
2025-06-30 14:56:31 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import Cabecalho from "./modulos/Cabecalho/cabecalho";
-import Lista from "./modulos/Lista/lista";
 import Navbar from "./modulos/NavBar/navbar";
 import Tarefa from "./modulos/Tarefas/tarefa";
 import 'bootstrap/dist/css/bootstrap.min.css';
